c3a630c7422ba54e6744f1bcd11be48285e11532
fakesanghyeok <s24066@gsm.hs.kr>
2024-10-15 23:18:16 +0900

    Redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
+
 function App() {
+  const count = useSelector((state) => state.count); // 상태 가져오기
+  const dispatch = useDispatch(); // 디스패치 함수 가져오기
+
   return (
-    <>
-    </>
+    <div>
+      <h1>카운터: {count}</h1>
+      <button onClick={() => dispatch(increment())}>증가</button>
+      <button onClick={() => dispatch(decrement())}>감소</button>
+    </div>
   );
 }
 
